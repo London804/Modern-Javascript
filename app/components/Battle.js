495dad97bdc69098d43e802eb56dd7f840c127c7
london804 <alexechaparro@gmail.com>
2018-07-05 15:06:44 -0400

    first commit. Finished battle.js and api.js

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,7 +1,7 @@
-var React = require('react');
-var PropTypes = require('prop-types');
-var Link = require('react-router-dom').Link;
-var PlayerPreview = require('./PlayerPreview');
+const React = require('react');
+const PropTypes = require('prop-types');
+const Link = require('react-router-dom').Link;
+const PlayerPreview = require('./PlayerPreview');
 
 class PlayerInput extends React.Component {
   constructor(props) {
@@ -14,13 +14,9 @@ class PlayerInput extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(event) {
-    var value = event.target.value;
+    const value = event.target.value;
 
-    this.setState(function () {
-      return {
-        username: value
-      }
-    });
+    this.setState(() => ({username: value }));
   }
   handleSubmit(event) {
     event.preventDefault();
@@ -31,21 +27,23 @@ class PlayerInput extends React.Component {
     );
   }
   render() {
+    const { username } = this.state;
+    const { label } = this.props;
     return (
       <form className='column' onSubmit={this.handleSubmit}>
-        <label className='header' htmlFor='username'>{this.props.label}</label>
+        <label className='header' htmlFor='username'>{label}</label>
         <input
           id='username'
           placeholder='github username'
           type='text'
-          value={this.state.username}
+          value={username}
           autoComplete='off'
           onChange={this.handleChange}
         />
         <button
           className='button'
           type='submit'
-          disabled={!this.state.username}>
+          disabled={!username}>
             Submit
         </button>
       </form>
@@ -75,28 +73,45 @@ class Battle extends React.Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  // ES5
+  // handleSubmit(id, username) {
+  //   this.setState(function () {
+  //     let newState = {};
+  //     newState[id + 'Name'] = username;
+  //     newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200}'
+  //     return newState;
+  //   });
+  // }
+
+  // ES6 computed property names
   handleSubmit(id, username) {
-    this.setState(function () {
-      var newState = {};
-      newState[id + 'Name'] = username;
-      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
-      return newState;
-    });
+    this.setState(() => ({
+      [id + 'Name']: username,
+      [id + 'Image']: `https://github.com/${username}.png?size=200}`
+    }))
   }
+
   handleReset(id) {
-    this.setState(function () {
-      var newState = {};
-      newState[id + 'Name'] = '';
-      newState[id + 'Image'] = null;
-      return newState;
-    })
-  }
+    this.setState(() => ({
+    [id + 'Name']: '',
+    [id + 'Image']: null
+    }))
+  };
+
+  // ES5
+  // render() {
+  //   var match = this.props.match;
+  //   var playerOneName = this.state.playerOneName;
+  //   var playerOneImage = this.state.playerOneImage;
+  //   var playerTwoName = this.state.playerTwoName;
+  //   var playerTwoImage = this.state.playerTwoImage;
+
+  // ES6 Destructuring 
+
   render() {
-    var match = this.props.match;
-    var playerOneName = this.state.playerOneName;
-    var playerOneImage = this.state.playerOneImage;
-    var playerTwoName = this.state.playerTwoName;
-    var playerTwoImage = this.state.playerTwoImage;
+    var { match } = this.props
+    const { playerOneName, playerOneImage, playerTwoName, playerTwoImage } = this.state
 
     return (
       <div>
@@ -114,7 +129,9 @@ class Battle extends React.Component {
               username={playerOneName}>
                 <button
                   className='reset'
-                  onClick={this.handleReset.bind(this, 'playerOne')}>
+                  // ES5 vs ES6 below
+                  // onClick={this.handleReset.bind(this, 'playerOne')}>
+                  onClick={ () =>this.handleReset('playerOne')}>
                     Reset
                 </button>
             </PlayerPreview>}
@@ -132,7 +149,7 @@ class Battle extends React.Component {
               username={playerTwoName}>
                 <button
                   className='reset'
-                  onClick={this.handleReset.bind(this, 'playerTwo')}>
+                  onClick={() => this.handleReset('playerTwo')}>
                     Reset
                 </button>
             </PlayerPreview>}
@@ -142,8 +159,8 @@ class Battle extends React.Component {
           <Link
             className='button'
             to={{
-              pathname: match.url + '/results',
-              search: '?playerOneName=' + playerOneName + '&playerTwoName=' + playerTwoName
+              pathname: `${match.url}/results`,
+              search: `?playerOneName='${playerOneName}'&playerTwoName='${playerTwoName}`
             }}>
               Battle
           </Link>}
--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,7 +1,7 @@
-var React = require('react');
-var PropTypes = require('prop-types');
+const React = require('react');
+const PropTypes = require('prop-types');
 
-var styles = {
+const styles = {
   content: {
     textAlign: 'center',
     fontSize: '35px'
@@ -17,22 +17,22 @@ class Loading extends React.Component {
     };
   }
   componentDidMount() {
-    var stopper = this.props.text + '...';
-    this.interval = window.setInterval(function () {
+    var stopper = `${this.props.text}'...'`;
+    this.interval = window.setInterval(() => {
       if (this.state.text === stopper) {
-        this.setState(function () {
+        this.setState(() => {
           return {
             text: this.props.text
-          }
-        })
+          };
+        });
       } else {
-        this.setState(function (prevState) {
+        this.setState((prevState) => {
           return {
             text: prevState.text + '.'
           }
         });
       }
-    }.bind(this), this.props.speed)
+    }, this.props.speed);
   }
   componentWillUnmount() {
     window.clearInterval(this.interval);
